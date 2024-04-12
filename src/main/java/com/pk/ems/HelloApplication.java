8761e34be4c2f7a87e60857f69f223a3a42c911d
rohitsmart <rohitsmart95@gmail.com>
2024-04-12 12:21:29 +0530

    commnet
    removed comment

--- src/main/java/com/pk/ems/HelloApplication.java
+++ src/main/java/com/pk/ems/HelloApplication.java
@@ -1,39 +1,9 @@
-// package com.pk.ems;
-
-// import java.io.IOException;
-
-// import javafx.application.Application;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Scene;
-// import javafx.stage.Stage;
-
-// public class HelloApplication extends Application {
-//     @SuppressWarnings("exports")
-//     @Override
-//     public void start(Stage stage) throws IOException {
-//         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//         stage.setTitle("Hello!");
-//         stage.setScene(scene);
-//         stage.show();
-//     }
-
-//     public static void main(String[] args) {
-//         launch();
-//     }
-// }
-
-
 package com.pk.ems;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.util.Enumeration;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
